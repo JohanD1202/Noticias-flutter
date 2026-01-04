f9d00a93f69f67b9c6ecb715727968bad679eafd
Johan Idárraga <idarragajohan8@gmail.com>
2026-01-03 19:30:06 -0500

    Add Article and Source domain entities

---
+++ lib/domain/entities/article.dart
@@ -0,0 +1,25 @@
+import 'package:noticias/domain/entities/source.dart';
+
+class Article {
+  final String id;
+  final String title;
+  final String author;
+  final String description;
+  final String content;
+  final String url;
+  final String imageUrl;
+  final DateTime publishedAt;
+  final Source source;
+
+  const Article({
+    required this.id,
+    required this.title,
+    required this.author,
+    required this.description,
+    required this.content,
+    required this.url,
+    required this.imageUrl,
+    required this.publishedAt,
+    required this.source,
+  });
+}
